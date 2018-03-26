bfc4d625bd575038d47783762a76c58994e5bc1c
fanatic84 <fanatic84@gmail.com>
2018-03-26 20:32:14 +0200

    Big refactor

---
+++ src/components/Tile.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="tileItem">
+        <span class="tileCount">{{tile.count}}</span>
+        <img class="item" :src="imgHref" v-on:click="click" v-on:dragstart="dragstart" draggable="true"></img>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        props: ['tile'],
+        methods: {
+            click() {
+                this.$emit('tile-click', {
+                    name: this.tile.name
+                });
+            },
+            dragstart(e) {
+                e.dataTransfer.setData('tile', JSON.stringify({
+                    name: this.tile.name
+                }))
+            }
+        },
+        computed: {
+            imgHref() {
+                return `img/${this.tile.name}.png`;
+            }
+        }
+    };
+</script>
